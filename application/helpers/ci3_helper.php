d3f94857abbf738f4a32390c2c825b3783ec3ace
SutiknoGo <sutiknogoutama97@gmail.com>
2020-09-07 22:39:44 +0700

    create access management in admin

--- application/helpers/ci3_helper.php
+++ application/helpers/ci3_helper.php
@@ -11,6 +11,7 @@ function is_logged_in()
         $menu = $ci->uri->segment(1);
 
         $queryMenu = $ci->db->get_where('user_menu', ['menu' => $menu])->row_array();
+
         $menu_id = $queryMenu['id'];
 
         $userAccess = $ci->db->get_where(
@@ -23,6 +24,19 @@ function is_logged_in()
 
         if ($userAccess->num_rows() < 1) {
             redirect('auth/blocked');
+        }
+    }
+
+    function check_access($role_id, $menu_id)
+    {
+        $ci = get_instance();
+
+        $ci->db->where('role_id', $role_id);
+        $ci->db->where('menu_id', $menu_id);
+        $result = $ci->db->get('user_access_menu');
+
+        if ($result->num_rows() > 0) {
+            return "checked='checked'";
         }
     }
 }
