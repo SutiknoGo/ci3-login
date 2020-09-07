e3f713375a1fb170c31f3bd9b8b51854b1a034d4
SutiknoGo <sutiknogoutama97@gmail.com>
2020-09-07 21:51:22 +0700

    update user access

--- application/helpers/ci3_helper.php
+++ application/helpers/ci3_helper.php
@@ -8,7 +8,7 @@ function is_logged_in()
         redirect('auth');
     } else {
         $role_id = $ci->session->userdata('role_id');
-        $menu = $ci->url->segment(1);
+        $menu = $ci->uri->segment(1);
 
         $queryMenu = $ci->db->get_where('user_menu', ['menu' => $menu])->row_array();
         $menu_id = $queryMenu['id'];
